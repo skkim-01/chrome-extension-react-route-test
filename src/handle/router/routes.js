6e24fe2827304b20d41bf01d9e4dd7864943ccba
bob kim <46100421+skkim-01@users.noreply.github.com>
2022-03-25 15:55:31 +0900

    add test codes for React.Component.....

--- src/handle/router/routes.js
+++ src/handle/router/routes.js
@@ -1,15 +1,20 @@
 import Page1 from "../../pages/page1"
 import Page2 from "../../pages/page2"
 import Home from "../../pages/home"
+import ClassComponent from "../../pages/page_class_component"
+import ClassComponentStatic from "../../pages/page_class_component_static"
 
 export class RouteConstants {
     static page_home = "/"
     static page_page1 = "/page1"
     static page_page2 = "/page2"
+    static page_class_component = "/page/class/component"
+    static page_static_component = "/page/static/component"
 }
 
 export default class RouteHandle {
     static dispatcherCB = null
+    static staticComponentClass = new ClassComponentStatic()
 
     // using only main
     static setDispatcherCB(cbFunc) {
@@ -23,10 +28,14 @@ export default class RouteHandle {
     // using only main
     static Dispatch(routeName) {
         switch ( routeName ) {
-            case RouteConstants.page_home:        return Home()
-            case RouteConstants.page_page1:       return Page1()
-            case RouteConstants.page_page2:       return Page2()
-            default:                              return Home()
+        case RouteConstants.page_home:                  return Home()
+        case RouteConstants.page_page1:                 return Page1()
+        case RouteConstants.page_page2:                 return Page2()
+        // when using React.Component class..
+        case RouteConstants.page_class_component:       return new ClassComponent().render()
+        // when using React.Component class as static variable
+        case RouteConstants.page_static_component:      return this.staticComponentClass.render()
+        default:                                        return Home()
         }
     }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import RouteHandle, { RouteConstants } from "../handle/router/routes"
 
-async function onClickPage1() {
+function onClickPage1() {
   RouteHandle.call(RouteConstants.page_page1)
 }
   
@@ -9,11 +9,21 @@ function onClickPage2() {
   RouteHandle.call(RouteConstants.page_page2)
 }
 
+function onClickPageClassComponent() {
+  RouteHandle.call(RouteConstants.page_class_component)
+}
+
+function onClickPageStaticComponent() {
+  RouteHandle.call(RouteConstants.page_static_component)
+}
+
 export default function Home() {
     return (
         <div className="App">
           <button onClick={onClickPage1}> Page1 </button>
           <button onClick={onClickPage2}> Page2 </button>
+          <button onClick={onClickPageClassComponent}> Page Class Component </button>
+          <button onClick={onClickPageStaticComponent}> Page Static Component </button>
         </div>
     );
 };
